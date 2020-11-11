d8f947ffb433ead5286128a412e2f366cfdcc17e
Ian Sealy <git@iansealy.com>
2020-11-11 01:05:58 +0000

    Fix Gmail locally with Firefox user agent.

--- recipes/gmail/index.js
+++ recipes/gmail/index.js
@@ -6,7 +6,7 @@ module.exports = Franz =>
       return [
         {
           headers: {
-            'user-agent': window.navigator.userAgent.replace(/(Ferdi|Electron)\/\S+ \([^)]+\)/g,"").trim(),
+            'user-agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:82.0) Gecko/20100101 Firefox/82.0',
           },
           requestFilters: {
             urls: ['*://*/*'],
